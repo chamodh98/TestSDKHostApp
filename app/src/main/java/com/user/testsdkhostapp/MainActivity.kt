f6176675840123e0c4119d6696277f9ea8e9ff95
Chamod Hettiarachchi <chamodbhanuka98@gmail.com>
2025-03-04 20:00:39 +0530

    update version

--- app/src/main/java/com/user/testsdkhostapp/MainActivity.kt
+++ app/src/main/java/com/user/testsdkhostapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.user.externalappopen.SDKAppOpen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
